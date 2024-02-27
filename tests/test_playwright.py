e6e096cd0554398f5f8399bec6c33fe258f2c9f4
Lawrence Good <lawrence.good55@gmail.com>
2024-02-26 20:05:53 -0800

    test retries

--- tests/test_playwright.py
+++ tests/test_playwright.py
@@ -1,9 +1,11 @@
+from typing import Union
 from unittest.mock import Mock, patch
 
 import pytest
-from playwright.async_api import Page
+from playwright.async_api import ElementHandle, Page
 
 from lowes.utils.playwright import get_el, navigate_to_page
+from lowes.utils.retry import retry
 
 
 @patch(
@@ -21,3 +23,20 @@ async def test_get_el_error_retries(mock_wait_for_selector: Mock, page: Page):
 async def test_navigate_to_page(page: Page):
     await navigate_to_page(page, "https://google.com")
     assert await page.title() == "Google"
+
+
+async def test_navigate_to_page_denied_retries(page: Page):
+    tracker = {"retry_count": 0}
+
+    @retry()
+    async def test_navigate_to_page(page, tracker) -> None:
+        await page.evaluate(f"document.write('<h1>Access Denied</h1>')")
+        if (
+            denied_el := await page.query_selector("body h1")
+        ) and await denied_el.text_content() == f"Access Denied":
+            tracker["retry_count"] += 1
+            raise Exception(f"Access denied")
+
+    with pytest.raises(Exception):
+        await test_navigate_to_page(page, tracker)
+    assert tracker["retry_count"] == 3
